a86f03d95290f70e8da71a77ceda0023cbfbf114
mikos8 <65290147+mikos8@users.noreply.github.com>
2020-10-31 00:15:30 +0600

    add header footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/alt-text */
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <div className='header-top'>
+          <div className="header-logo">
+            <p>ComInfo</p>
+          </div>
+          
+          <div className="header-tel">
+            <p>87059727904</p>
+          </div>
+        </div>
+      
+        <div className="header-menu">
+          <ul>
+            <li>
+              <a href="" className="menu-link">Главная</a>
+            </li>
+            <li><a href="" className="menu-link">Услуги</a></li>
+            <li>
+              <a href="" className="menu-link">
+                Контакты
+              </a>     
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+      <div className="container">
+      </div>
+      <div class="footer">
+      <div class="container">
+        <div class="footer-row">
+          <div class="footer-copyright">
+            <div class="footer-copyright-name">COMINFO</div>
+            <p>Сопровождение вашего бизнеса</p>
+          </div>
+          <div class="footer-icons">
+            <p>Мы в социальных сетях</p>
+            <div class="footer-icon-link">
+              <a href="#"><img src="./img/footer/facebook-f 1.svg" /></a>
+              <a href="#"><img src="./img/footer/instagram 1.svg" /></a>
+              <a href="#"><img src="./img/footer/linkedin-in-brands.svg" /></a>
+              <a href="#"><img src="./img/footer/github-brands.svg" /></a>
+              <a href="#"><img src="./img/footer/vk-brands 1.svg" /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      
+      
+      
     </div>
   );
 }
