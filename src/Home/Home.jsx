24a8a87a8fb6c4ceee91b4d18a90553fd139a8fc
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2020-10-31 17:26:15 +0600

    component + com/css

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
     return (
     <div className="home">
       <div className="home-slider">
           <img className="home-slider-img" src="https://png.pngtree.com/thumb_back/fw800/back_our/20190625/ourmid/pngtree-minimalistic-geometric-triangles-background-image_259751.jpg" alt="picture"/>
-          
+          <form action="" className="home-slider-form">
+              <p>Отправить заявку</p>
+            <label className="form-item-text">Ваше имя</label>
+            <input type="text" placeholder="Введите имя"/><br/>
+            <label className="form-item-text">Ваш телефон</label>
+            <input type="text" placeholder="Номер телефона"/><br/>
+            <label className="form-item-text">Ваш email</label>
+            <input type="text" placeholder="email"/><br/>
+            
+            <button>Отправить</button>
+          </form>
       </div>
       <div className="home-about">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nemo ipsum dolores beatae? Nisi aliquam eligendi veniam neque a exercitationem, maiores facere temporibus obcaecati in nostrum, accusantium aspernatur deleniti voluptate!</p>
---
+++ src/contacts/contacts.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./contacts.css";
+
+const Contacts = () => {
+    return (
+    <div className="home">
+      <div className="home-slider">
+          
+          <form action="" className="home-slider-form">
+              <p>Отправить заявку</p>
+            <label className="form-item-text">Ваше имя</label>
+            <input type="text" placeholder="Введите имя"/><br/>
+            <label className="form-item-text">Ваш телефон</label>
+            <input type="text" placeholder="Номер телефона"/><br/>
+            <label className="form-item-text">Ваш email</label>
+            <input type="text" placeholder="email"/><br/>
+            
+            <button>Отправить</button>
+          </form>
+      </div>
+      <div className="home-about">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nemo ipsum dolores beatae? Nisi aliquam eligendi veniam neque a exercitationem, maiores facere temporibus obcaecati in nostrum, accusantium aspernatur deleniti voluptate!</p>
+      </div>
+      <div className="home-text">
+        <div className="home-text-items">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste optio vero facere eum eos ex aliquid deserunt nobis molestiae minima ab quod assumenda, doloremque porro in, rerum vitae amet necessitatibus?</div>
+        <div className="home-text-items">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste optio vero facere eum eos ex aliquid deserunt nobis molestiae minima ab quod assumenda, doloremque porro in, rerum vitae amet necessitatibus?</div>
+      </div>
+    </div>
+    );
+  };
+  
+  export default Contacts;
